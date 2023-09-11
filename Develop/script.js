c4640b08a1a443a72e05336a4590fc1ee204bfc4
InternetEthan <internetethan@gmail.com>
2023-09-11 11:57:54 -0500

    added click event; records planner hour/text

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,7 @@
 // in the html.
 $(function () {
 const currentDay = dayjs();
-console.log('currentday')
+console.log(currentDay);
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -11,12 +11,6 @@ console.log('currentday')
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
@@ -37,7 +31,23 @@ $(".time-block").each(function(index, value) {
   console.log(scheduleHour);
   console.log(currentHour);
 
+  // button listener
+  value.addEventListener("click", function (event) {
+    if (event.target.matches('button')) {
+      console.log("clicked");
+      console.log(event.target);
+      saveSchedule(event);
+    }
+  });
 });
 
-  $("#currentDay").text(currentDay.format('dddd, MMMM D'))
+function saveSchedule(event) {
+  const hour = event.target.parentElement.getAttribute("data-hour");
+  const text = event.target.parentElement.querySelector("textarea").value;
+  console.log("hour", hour);
+  console.log("text", text);
+}
+
+  $("#currentWeekDay").text(currentDay.format('dddd'));
+  $("#currentDay").text(currentDay.format('MMMM DD, YYYY'));
 });
