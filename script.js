4dd245a67b62d606cb20515779364dd69eb542a8
InternetEthan <internetethan@gmail.com>
2023-09-12 21:39:03 -0500

    misc edits to comments

--- script.js
+++ script.js
@@ -1,27 +1,12 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
 const currentDay = dayjs();
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 $(".time-block").each(function(index, timeBlock) {
   const schedules = JSON.parse(localStorage.getItem('schedules')) || [];
   const scheduleHour = timeBlock.getAttribute('data-hour');
   const currentHour = dayjs().hour();
   
-  
+  // if statement to check time and change style accordingly
   if (scheduleHour > currentHour) {
     $(timeBlock).removeClass("past present").addClass("future");
   } else if(scheduleHour < currentHour) {
@@ -55,7 +40,7 @@ function saveSchedule(event) {
   console.log("hour", hour);
   console.log("text", text);
   console.log("schedule", newSchedule);
-
+  
   if (localStorage.getItem("schedules")) {
     const schedules = JSON.parse(localStorage.getItem("schedules"));
 
@@ -78,7 +63,7 @@ schedules.splice(indexRmv,1)
 }
 
 };
-
+// adding current day text data to header
 $("#currentWeekDay").text(currentDay.format('dddd'));
 $("#currentDay").text(currentDay.format('MMMM DD, YYYY'));
 
